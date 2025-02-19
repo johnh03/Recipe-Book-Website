73d909a8bd321f1a58ea950564fc405f1a57c8e4
johnh <johnhuang0330@gmail.com>
2025-02-19 12:07:32 -0500

    Implemented Login and Signup pages and features

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # super-simple-website
 A super simple website using HTML, JavaScript, and CSS. This project eventually transformed into a website where the user will be able to post their recipes with a list of ingredients, steps, and an image of their complete dish.
+
+Break down of the project:
+
+Home Page: The users will be able to post their recipes with a name, list of ingredients, as well as step by step instructions. An upload image option is also available.
+
+Signup Page: The user will be able to sign up when pressing the "Login / Singup" button then pressing the "Sign up here" hyper link next to "Don't have an account?". There the user will be able to create a username and password. The username and password are currently being stored in the users local browser. Therefore if the user clears their browser cashe then the account information would be deleted with it.
+
+Login Page: The user will be able to login when pressing the "Login/ Singup" button. Here the user will have to put in their username and password to gain access to their account. Not much has been accomplished with accounts, the future plan is to be able to search for a username and all of their recipes will show up.
--- script.js
+++ script.js
@@ -1,4 +1,92 @@
 // script.js
+
+// Helper function to get and set local storage data
+function getUsers() {
+    return JSON.parse(localStorage.getItem('users')) || [];
+}
+
+function setUsers(users) {
+    localStorage.setItem('users', JSON.stringify(users));
+}
+
+function getCurrentUser() {
+    return localStorage.getItem('currentUser');
+}
+
+function setCurrentUser(username) {
+    localStorage.setItem('currentUser', username);
+}
+
+function clearCurrentUser() {
+    localStorage.removeItem('currentUser');
+}
+
+// Sign-up functionality
+document.querySelector('#signup-form')?.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username = document.querySelector('#signup-username').value;
+    const password = document.querySelector('#signup-password').value;
+
+    let users = getUsers();
+
+    if (users.some(user => user.username === username)) {
+        alert('Username already exists!');
+    } else {
+        users.push({ username, password });
+        setUsers(users);
+        alert('Account created successfully!');
+        window.location.href = 'index.html';
+    }
+});
+
+// Login functionality
+document.querySelector('#login-form')?.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const username = document.querySelector('#login-username').value;
+    const password = document.querySelector('#login-password').value;
+
+    const users = getUsers();
+    const user = users.find(user => user.username === username && user.password === password);
+
+    if (user) {
+        setCurrentUser(username);
+        alert('Login successful!');
+        window.location.href = 'index.html';
+    } else {
+        alert('Invalid username or password!');
+    }
+});
+
+// Display user info on home page
+function updateUserUI() {
+    const userInfo = document.querySelector('#user-info');
+    const authButton = document.querySelector('#auth-button');
+
+    const currentUser = getCurrentUser();
+
+    if (currentUser) {
+        userInfo.textContent = `Logged in as: ${currentUser}`;
+        authButton.textContent = 'Sign Out';
+    } else {
+        userInfo.textContent = '';
+        authButton.textContent = 'Login / Sign Up';
+    }
+}
+
+// Auth button functionality
+document.querySelector('#auth-button')?.addEventListener('click', function() {
+    if (getCurrentUser()) {
+        clearCurrentUser();
+        alert('Signed out successfully!');
+        updateUserUI();
+    } else {
+        window.location.href = 'login.html';
+    }
+});
+
+// Initialize UI on page load
+document.addEventListener('DOMContentLoaded', updateUserUI);
+
 document.getElementById('add-recipe').addEventListener('click', function(event) {
     event.preventDefault();
 
@@ -35,3 +123,4 @@ document.getElementById('add-recipe').addEventListener('click', function(event) 
     document.getElementById('recipe-image').value = '';
 }
 );
+
